37cbab5a20306036088d8f9d30d33b42454e312c
Tom Harvell <harvellt@gmail.com>
2019-11-16 13:43:14 -0500

    Working through the tutorial

--- render_functions.py
+++ render_functions.py
@@ -6,7 +6,32 @@ def draw_entity(con, entity):
     libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
 
 
-def render_all(con, entities, screen_width, screen_height):
+ def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors):
+    # Draw all the tiles in the game map
+    if fov_recompute:     
+        for y in range(game_map.height):
+            for x in range(game_map.width):
+                visible = libtcod.map_is_in_fov(fov_map, x, y)
+                wall = game_map.tiles[x][y].block_sight
+                if visible:
+                    if wall:
+                       libtcod.console_set_char_background(con, x, y, colors.get('light_wall'), libtcod.BKGND_SET)
+                    else:
+                       libtcod.console_set_char_background(con, x, y, colors.get('light_ground'), libtcod.BKGND_SET)
+                else:
+                    if wall:
+                       libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
+                    else:
+                       libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+                if wall:
+                    libtcod.console_set_char_background(
+                    con, x, y, colors.get("dark_wall"), libtcod.BKGND_SET
+                )
+                else:
+                    libtcod.console_set_char_background(
+                    con, x, y, colors.get("dark_ground"), libtcod.BKGND_SET
+                )
+
     # Draw all entities in the list
     for entity in entities:
         draw_entity(con, entity)
